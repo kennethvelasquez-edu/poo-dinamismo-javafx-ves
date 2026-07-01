3bae7d201e8dedc8dbe16b35cee3a052ff9ae7ab
Kenneth Velasquez <kennethvelasquez@kinal.edu.gt>
2026-07-01 14:01:06 -0600

    Se cambio el nombre del paquete system con mi nombre

--- poo-dinamismo-javafx-ves/src/com/kennethv/system/Principal.java
+++ poo-dinamismo-javafx-ves/src/com/kennethv/system/Principal.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package com.kennethvelasquez.system;
+package com.kennethv.system;
 
 import com.kennethvelasquez.controller.SceneManagerController;
 import javafx.application.Application;
