da0b7123cd077d0c62c166398d0c39c33f4e7256
Kenneth Velasquez <kennethvelasquez@kinal.edu.gt>
2026-07-04 15:28:44 -0600

    Se logra agregar Personas a la lista y limpiar el formulario

--- poo-dinamismo-javafx-ves/src/com/kennethvelasquez/controller/RegistroController.java
+++ poo-dinamismo-javafx-ves/src/com/kennethvelasquez/controller/RegistroController.java
@@ -9,7 +9,9 @@ import java.util.ArrayList;
 import com.kennethvelasquez.model.Persona;
 import com.kennethvelasquez.model.Estudiante;
 import com.kennethvelasquez.model.Maestro;
+import com.kennethvelasquez.view.ResumenView;
 import javafx.collections.FXCollections;
+import javax.swing.JOptionPane;
 
 public class RegistroController {
     private final RegistroView registroView;
@@ -33,9 +35,6 @@ public class RegistroController {
         
         listaRegistroPersonas = new ArrayList<>();
         
-        listaRegistroPersonas.add(
-                new Estudiante("4to", 123, true, "Juanito", "Panchito")
-        );
     }
     public void construirComboBox(){
         this.registroView.getCmbTipoPersona().setItems(
@@ -43,7 +42,133 @@ public class RegistroController {
         );
     }
     public void construirAcciones(){
+        this.registroView.getCmbTipoPersona().setOnAction(
+           (evento ) ->{
+               validarCampos(this.registroView.getCmbTipoPersona().getValue());
+           }
+        );
         
+        this.registroView.getBtnGuardar().setOnAction(
+           (evento ) ->{
+               guardarPersona();
+           }
+        );
+        
+        this.registroView.getBtnLimpiar().setOnAction(
+            (evento) -> {
+                limpiarCampos();
+                mostrarCampos();
+            }
+        );
+    }
+    
+    public void limpiarCampos() {
+        this.registroView.getTxtNombres().clear();
+        this.registroView.getTxtApellidos().clear();
+        this.registroView.getTxtCurso().clear();
+        this.registroView.getTxtNumeroCarne().clear();
+        this.registroView.getTxtGradoEstudiante().clear();
+        this.registroView.getTxtGradoMaestro().clear();
+        this.registroView.getRbAprabado().setSelected(false);
+        this.registroView.getCbActivo().setSelected(false);
+        this.registroView.getCmbTipoPersona().getSelectionModel().select(null);
+    }
+    
+    public void mostrarCampos() {
+        //Mostramos los campos de estudiante
+        this.registroView.getLblAprobado().setVisible(true);
+        this.registroView.getLblNumeroCarne().setVisible(true);
+        this.registroView.getLblGradoEstudiante().setVisible(true);
+        //TextField
+        this.registroView.getTxtNumeroCarne().setVisible(true);
+        this.registroView.getTxtGradoEstudiante().setVisible(true);
+        //RadioButton
+        this.registroView.getRbAprabado().setVisible(true);
+
+        //Mostramos los campos de maestros
+        this.registroView.getLblActivo().setVisible(true);
+        this.registroView.getLblCurso().setVisible(true);
+        this.registroView.getLblGradoMaestro().setVisible(true);
+        //TextField
+        this.registroView.getTxtCurso().setVisible(true);
+        this.registroView.getTxtGradoMaestro().setVisible(true);
+        //RadioButton
+        this.registroView.getRbAprabado().setVisible(true);
+    }
+    
+    public void guardarPersona(){
+        if( this.registroView.getCmbTipoPersona().getValue() == null ){
+            JOptionPane.showMessageDialog(null, "TIENES QUE ELEGIR EL OBJETO A REGISTRAR");
+            return;
+        }
+        if( this.registroView.getCmbTipoPersona().getValue() instanceof Estudiante ){
+            Estudiante nuevoEstudiante = new Estudiante();
+            nuevoEstudiante.setNombres( this.registroView.getTxtNombres().getText() );
+            nuevoEstudiante.setApellidos( this.registroView.getTxtApellidos().getText() );
+            nuevoEstudiante.setAprobado( this.registroView.getRbAprabado().isSelected() );
+            nuevoEstudiante.setNumeroCarne( Integer.parseInt( this.registroView.getTxtNumeroCarne().getText() ) );
+            nuevoEstudiante.setGrado(this.registroView.getTxtGradoEstudiante().getText());
+            listaRegistroPersonas.add(nuevoEstudiante);
+        }else{
+             Maestro nuevoMaestro = new Maestro();
+            nuevoMaestro.setNombres( this.registroView.getTxtNombres().getText() );
+            nuevoMaestro.setApellidos( this.registroView.getTxtApellidos().getText() );
+            nuevoMaestro.setActivo(this.registroView.getCbActivo().isSelected() );
+            nuevoMaestro.setCurso(this.registroView.getTxtCurso().getText()  );
+            nuevoMaestro.setGrado(this.registroView.getTxtGradoEstudiante().getText());
+            listaRegistroPersonas.add(nuevoMaestro);
+        }
+        //Cada que agrego a la lista, independientemente del objeto actualizare la lista en el resument
+        new ResumenController(ResumenView.getInstanciaResumenView()).cargarDatos();
+        
+    }
+    
+    public void validarCampos(Persona objetoPersonaSeleccionado ){
+        if (objetoPersonaSeleccionado == null) {
+            return;
+        }
+        String claseSeleccionada = objetoPersonaSeleccionado.toString();
+        if( claseSeleccionada.equals("Estudiante")  ){
+            //LLAMANDO A LOS LABEL y ocultandolo
+            this.registroView.getLblGradoMaestro().setVisible(false);
+            this.registroView.getLblCurso().setVisible(false);
+            this.registroView.getLblActivo().setVisible(false);
+            //TEXTFIELD
+            this.registroView.getTxtGradoMaestro().setVisible(false);
+            this.registroView.getTxtCurso().setVisible(false);
+            //CHECBOX
+            this.registroView.getCbActivo().setVisible(false);
+            
+            //Mostramos los campos de estudiante
+            this.registroView.getLblAprobado().setVisible(true);
+            this.registroView.getLblNumeroCarne().setVisible(true);
+            this.registroView.getLblGradoEstudiante().setVisible(true);
+            //TextField
+            this.registroView.getTxtNumeroCarne().setVisible(true);
+            this.registroView.getTxtGradoEstudiante().setVisible(true);
+            //RadioButton
+            this.registroView.getRbAprabado().setVisible(true);
+        }else{
+            //LLAMANDO A LOS LABEL y ocultandolo
+            this.registroView.getLblGradoEstudiante().setVisible(false);
+            this.registroView.getLblNumeroCarne().setVisible(false);
+            this.registroView.getLblAprobado().setVisible(false);
+            //TEXTFIELD
+            this.registroView.getTxtGradoEstudiante().setVisible(false);
+            this.registroView.getTxtNumeroCarne().setVisible(false);
+            //CHECBOX
+            this.registroView.getRbAprabado().setVisible(false);
+            
+            //Mostramos los campos de maestros
+            this.registroView.getLblActivo().setVisible(true);
+            this.registroView.getLblCurso().setVisible(true);
+            this.registroView.getLblGradoMaestro().setVisible(true);
+            //TextField
+            this.registroView.getTxtCurso().setVisible(true);
+            this.registroView.getTxtGradoMaestro().setVisible(true);
+            //RadioButton
+            this.registroView.getCbActivo().setVisible(true);
+        }
     }
 
     public static ArrayList<Persona> getListaRegistroPersonas() {
--- poo-dinamismo-javafx-ves/src/com/kennethvelasquez/view/RegistroView.java
+++ poo-dinamismo-javafx-ves/src/com/kennethvelasquez/view/RegistroView.java
@@ -93,6 +93,7 @@ public class RegistroView {
         formulario.add(lblApellidos, 0, 1);
         formulario.add(txtApellidos, 1, 1);
 
+        // Estudiante
         formulario.add(lblGradoEstudiante, 0, 2);
         formulario.add(txtGradoEstudiante, 1, 2);
 
@@ -102,15 +103,16 @@ public class RegistroView {
         formulario.add(lblAprobado, 0, 4);
         formulario.add(rbAprabado, 1, 4);
 
+        // Estudiante
         formulario.add(lblGradoMaestro, 0, 5);
         formulario.add(txtGradoMaestro, 1, 5);
 
         formulario.add(lblCurso, 0, 6);
         formulario.add(txtCurso, 1, 6);
-
+        
         formulario.add(lblActivo, 0, 7);
         formulario.add(cbActivo, 1, 7);
-
+        
         formulario.add(btnGuardar, 0, 8);
         formulario.add(btnLimpiar, 1, 8);
 
@@ -162,4 +164,149 @@ public class RegistroView {
     public Pane getInstanciaPanel() {
         return instanciaPanel;
     }
+
+    public Label getLblNombres() {
+        return lblNombres;
+    }
+
+    public void setLblNombres(Label lblNombres) {
+        this.lblNombres = lblNombres;
+    }
+
+    public Label getLblApellidos() {
+        return lblApellidos;
+    }
+
+    public void setLblApellidos(Label lblApellidos) {
+        this.lblApellidos = lblApellidos;
+    }
+
+    public TextField getTxtNombres() {
+        return txtNombres;
+    }
+
+    public void setTxtNombres(TextField txtNombres) {
+        this.txtNombres = txtNombres;
+    }
+
+    public TextField getTxtApellidos() {
+        return txtApellidos;
+    }
+
+    public void setTxtApellidos(TextField txtApellidos) {
+        this.txtApellidos = txtApellidos;
+    }
+
+    public Label getLblGradoEstudiante() {
+        return lblGradoEstudiante;
+    }
+
+    public void setLblGradoEstudiante(Label lblGradoEstudiante) {
+        this.lblGradoEstudiante = lblGradoEstudiante;
+    }
+
+    public Label getLblNumeroCarne() {
+        return lblNumeroCarne;
+    }
+
+    public void setLblNumeroCarne(Label lblNumeroCarne) {
+        this.lblNumeroCarne = lblNumeroCarne;
+    }
+
+    public Label getLblAprobado() {
+        return lblAprobado;
+    }
+
+    public void setLblAprobado(Label lblAprobado) {
+        this.lblAprobado = lblAprobado;
+    }
+
+    public TextField getTxtGradoEstudiante() {
+        return txtGradoEstudiante;
+    }
+
+    public void setTxtGradoEstudiante(TextField txtGradoEstudiante) {
+        this.txtGradoEstudiante = txtGradoEstudiante;
+    }
+
+    public TextField getTxtNumeroCarne() {
+        return txtNumeroCarne;
+    }
+
+    public void setTxtNumeroCarne(TextField txtNumeroCarne) {
+        this.txtNumeroCarne = txtNumeroCarne;
+    }
+
+    public RadioButton getRbAprabado() {
+        return rbAprabado;
+    }
+
+    public void setRbAprabado(RadioButton rbAprabado) {
+        this.rbAprabado = rbAprabado;
+    }
+
+    public Label getLblActivo() {
+        return lblActivo;
+    }
+
+    public void setLblActivo(Label lblActivo) {
+        this.lblActivo = lblActivo;
+    }
+
+    public Label getLblCurso() {
+        return lblCurso;
+    }
+
+    public void setLblCurso(Label lblCurso) {
+        this.lblCurso = lblCurso;
+    }
+
+    public Label getLblGradoMaestro() {
+        return lblGradoMaestro;
+    }
+
+    public void setLblGradoMaestro(Label lblGradoMaestro) {
+        this.lblGradoMaestro = lblGradoMaestro;
+    }
+
+    public TextField getTxtGradoMaestro() {
+        return txtGradoMaestro;
+    }
+
+    public void setTxtGradoMaestro(TextField txtGradoMaestro) {
+        this.txtGradoMaestro = txtGradoMaestro;
+    }
+
+    public TextField getTxtCurso() {
+        return txtCurso;
+    }
+
+    public void setTxtCurso(TextField txtCurso) {
+        this.txtCurso = txtCurso;
+    }
+
+    public CheckBox getCbActivo() {
+        return cbActivo;
+    }
+
+    public void setCbActivo(CheckBox cbActivo) {
+        this.cbActivo = cbActivo;
+    }
+
+    public Button getBtnGuardar() {
+        return btnGuardar;
+    }
+
+    public void setBtnGuardar(Button btnGuardar) {
+        this.btnGuardar = btnGuardar;
+    }
+
+    public Button getBtnLimpiar() {
+        return btnLimpiar;
+    }
+
+    public void setBtnLimpiar(Button btnLimpiar) {
+        this.btnLimpiar = btnLimpiar;
+    }
+    
 }
